e448cc21bb6d759678b156e31d5fb01ad0e5b2e6
imascarenho <tu_email@ejemplo.com>
2024-09-09 18:58:53 -0300

    Add .gitignore to ignore SSH config

---
+++ TP1.py
@@ -0,0 +1,16 @@
+import random
+num = random.random()
+print(num)
+def mi_funcion(parametro1, parametro2):
+    # Implementa la lógica de la función
+    resultado = parametro1 + parametro2
+    return resultado
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+contador = 0
+print("Mi primera función es para realizar sumas.")
+while contador < 10:
+    # Código a ejecutar en cada iteración
+    a=random.choice(list1)
+    b=random.choice(list1)
+    print("Esta es la suma ", contador + 1, ". Voy a sumar ", a, " + ", b, " es igual a", mi_funcion(a,b))
+    contador += 1  # Incrementa el contador
